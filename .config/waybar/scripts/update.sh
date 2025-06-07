fa32cedaaea15fdf978576582df9a5f0becdce1d
wartuu <michu4154@gmail.com>
2025-06-07 17:09:58 +0200

    update

--- .config/waybar/scripts/update.sh
+++ .config/waybar/scripts/update.sh
@@ -8,5 +8,5 @@ if [[ $count -eq 0 ]]; then
     echo '{"text": " ", "tooltip": "System is up to date!"}'
 else
     tooltip=$(echo "$updates" | sed ':a;N;$!ba;s/\n/\\n/g')
-    echo "{\"text\": \"$count󰚰 \", \"tooltip\": \"$tooltip\"}"
+    echo "{\"text\": \"$count 󰚰 \", \"tooltip\": \"$tooltip\"}"
 fi
